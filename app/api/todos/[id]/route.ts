2ac99d144f54b2275e67166175fcfa7bdb4f1747
Gobinda Das <dasgobinda9955@gmail.com>
2025-06-10 21:00:55 +0530

    feat: add PUT endpoint to update todo completion status with user authorization

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -3,6 +3,62 @@ import { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 
 
+
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        
+        const { userId } = await auth();
+
+        if (!userId) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        }
+
+        const { completed } = await req.json();
+        const todoId = params.id;
+
+        if (!todoId) {
+            return NextResponse.json({ error: "Missing todo id" }, { status: 400 });
+        }
+
+
+        const todo = await prisma.todo.findUnique({
+            where: { id: todoId },
+        })
+
+        if (!todo) {
+            return NextResponse.json({ error: "Todo not found" }, { status: 404 });
+        }
+
+
+        if (todo.userId !== userId) {
+            return NextResponse.json({ error: "Forbidden" }, { status: 401 });
+        }
+
+
+        const updatedTodo = await prisma.todo.update({
+            where: { id: todoId },
+            data: { completed: completed },
+        })
+
+        if (!updatedTodo) {
+            return NextResponse.json({ error: "Failed to update todo" }, { status: 500 });
+        }
+
+        
+
+        return NextResponse.json(
+            { 
+                todo: updatedTodo,
+                msg: "Todo updated successfully" 
+            }, 
+            { status: 200 }
+        )
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 401 });
+    }
+}
+
+
 export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
     try {
         
@@ -36,7 +92,7 @@ export async function DELETE(req: NextRequest, { params }: { params: { id: strin
         if (!todo) {
             return NextResponse.json({ error: "Todo not found" }, { status: 404 });
         }
-        
+
 
         if (todo.userId !== user.id) {
             return NextResponse.json({ error: "Forbidden" }, { status: 401 });
